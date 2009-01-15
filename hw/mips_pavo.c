4147866ecb2954cbc3ee612ade30d3a106bc9831
yajin <yajin@vm-kernel.org>
2009-01-15 13:54:58 +0800

    change the copyright info 2008 to 2009

--- hw/mips_pavo.c
+++ hw/mips_pavo.c
@@ -1,7 +1,7 @@
 /*
  * QEMU pavo demo board emulation
  *
- * Copyright (c) 2008 yajin (yajin@vm-kernel.org)
+ * Copyright (c) 2009 yajin (yajin@vm-kernel.org)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
